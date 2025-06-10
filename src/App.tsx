574ccc38d935e81f79c191c614cd83ea63cbed82
robertmayo <robertmayoe@gmail.com>
2025-06-10 15:51:06 +0800

    Setup tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <>
-      <h1>Hello World</h1>
+      <h1 className="text-lg text-green-100">Hello World</h1>
       <p>This is the paragraph section</p>
     </>
   );
