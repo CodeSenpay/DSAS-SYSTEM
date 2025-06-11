c7e0cfec8cc84403802f55b77919a1d2b0605498
robertmayo <robertmayoe@gmail.com>
2025-06-11 10:10:34 +0800

    Setup Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,38 @@
+import { lazy, Suspense } from "react";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Navigate,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import LoginPage from "./pages/LoginPage";
+const DashboardPage = lazy(() => import("./pages/DashboardPage"));
 function App() {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route>
+        <Route path="/" element={<Navigate to="login" replace />} />
+        <Route path="login" element={<LoginPage />} />
+        <Route path="dashboard" element={<DashboardPage />} />
+      </Route>
+    )
+  );
   return (
     <>
-      <h1 className="text-lg text-green-500">Hello World</h1>
-      <p>This is the paragraph section</p>
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        closeButton={false}
+        rtl={false}
+        pauseOnHover
+        draggable
+      />
+      <Suspense fallback={<h1>Loading...</h1>}>
+        <RouterProvider router={router} />
+      </Suspense>
     </>
   );
 }
---
+++ src/pages/DashboardPage.tsx
@@ -0,0 +1,7 @@
+export default function DashboardPage() {
+  return (
+    <div className="max-w-screen h-screen bg-[#f0f2f5]">
+      <h1>Welcome User!</h1>
+    </div>
+  );
+}
