f18b4bb60abfde2c320326f097467904a0e1b349
Maklizy <126657213+MarklanHampac@users.noreply.github.com>
2025-06-23 14:20:55 +0800

    feat: Implement user registration functionality and add nodemailer dependency

--- node-api/routes.js
+++ node-api/routes.js
@@ -1,6 +1,7 @@
 import cookieParser from "cookie-parser";
 import { Router } from "express";
 import { login ,logout} from "./controllers/login-controller.js";
+import { register } from "./controllers/register-controller.js";
 import { handle_schedule } from "./controllers/schedule-main-controller.js";
 import { authenticate } from "./middleware/middleware.js";
 
@@ -12,5 +13,6 @@ router.post("/api/scheduling-system/user", handle_schedule); //temporary
 router.get("/api/reporting-system", authenticate, handle_schedule);
 router.post("/api/login", login);
 router.post("/api/logout", logout);
+router.post("/api/register", register);
 
 export default router;
